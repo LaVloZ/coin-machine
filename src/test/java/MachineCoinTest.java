bc4d29baac9823eea015f0a2b7a5418af35b8829
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-26 05:36:07 +0100

    refacto

--- src/test/java/MachineCoinTest.java
+++ src/test/java/MachineCoinTest.java
@@ -4,21 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MachineCoinTest {
     @Test
-    public void piece_2_for_value_2() {
+    public void one_piece_2_for_value_2() {
         int piece2 = change2(2);
 
         assertThat(piece2).isEqualTo(1);
     }
 
     @Test
-    public void piece_2_for_value_7() {
+    public void one_piece_2_for_value_7() {
         int piece2 = change2(2);
 
         assertThat(piece2).isEqualTo(1);
     }
 
     @Test
-    public void piece_2_for_value_12() {
+    public void one_piece_2_for_value_12() {
         int piece2 = change2(2);
 
         assertThat(piece2).isEqualTo(1);
@@ -53,56 +53,56 @@ public class MachineCoinTest {
     }
 
     @Test
-    public void two_pieces_3_for_value_6() {
+    public void three_pieces_2_for_value_6() {
         int piece2 = change2(6);
 
         assertThat(piece2).isEqualTo(3);
     }
 
     @Test
-    public void two_pieces_3_for_value_11() {
+    public void three_pieces_2_for_value_11() {
         int piece2 = change2(11);
 
         assertThat(piece2).isEqualTo(3);
     }
 
     @Test
-    public void two_pieces_3_for_value_16() {
+    public void three_pieces_2_for_value_16() {
         int piece2 = change2(11);
 
         assertThat(piece2).isEqualTo(3);
     }
 
     @Test
-    public void two_pieces_3_for_value_21() {
+    public void three_pieces_2_for_value_21() {
         int piece2 = change2(11);
 
         assertThat(piece2).isEqualTo(3);
     }
 
     @Test
-    public void two_pieces_4_for_value_8() {
+    public void four_pieces_2_for_value_8() {
         int piece2 = change2(8);
 
         assertThat(piece2).isEqualTo(4);
     }
 
     @Test
-    public void two_pieces_4_for_value_13() {
+    public void four_pieces_2_for_value_13() {
         int piece2 = change2(13);
 
         assertThat(piece2).isEqualTo(4);
     }
 
     @Test
-    public void two_pieces_4_for_value_18() {
+    public void four_pieces_2_for_value_18() {
         int piece2 = change2(18);
 
         assertThat(piece2).isEqualTo(4);
     }
 
     @Test
-    public void two_pieces_4_for_value_23() {
+    public void four_pieces_2_for_value_23() {
         int piece2 = change2(23);
 
         assertThat(piece2).isEqualTo(4);
