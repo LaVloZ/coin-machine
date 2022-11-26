71f3b403243629f90d3e9d5b3b2df2ca02c10184
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-26 05:09:11 +0100

    refacto

--- src/test/java/MachineCoinTest.java
+++ src/test/java/MachineCoinTest.java
@@ -17,6 +17,13 @@ public class MachineCoinTest {
         assertThat(changedMoney.piece2).isEqualTo(1);
     }
 
+    @Test
+    public void piece_2_for_value_12() {
+        Change changedMoney = change(2);
+
+        assertThat(changedMoney.piece2).isEqualTo(1);
+    }
+
     private Change change(int value) {
         return new Change(1, 0, 0);
     }
