ac544ba3aa3dde5c7b4ddac821f13141ae820bfe
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-26 06:21:17 +0100

    refacto

--- src/test/java/MachineCoinTest.java
+++ src/test/java/MachineCoinTest.java
@@ -68,7 +68,7 @@ public class MachineCoinTest {
 
     private int change5(int value) {
         int lastDigit = value % 10;
-        if (lastDigit == 5 || lastDigit == 7 || lastDigit == 9 || lastDigit == 1) return 1;
+        if (lastDigit == 1 || lastDigit == 5 || lastDigit == 7 || lastDigit == 9) return 1;
         return 0;
     }
 
