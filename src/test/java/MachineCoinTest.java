f25dcfcf4f38d9fe19aa43dadd218f194045356e
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-26 05:07:42 +0100

    one piece 2 for 7

--- src/test/java/MachineCoinTest.java
+++ src/test/java/MachineCoinTest.java
@@ -10,6 +10,13 @@ public class MachineCoinTest {
         assertThat(changedMoney.piece2).isEqualTo(1);
     }
 
+    @Test
+    public void piece_2_for_value_7() {
+        Change changedMoney = change(2);
+
+        assertThat(changedMoney.piece2).isEqualTo(1);
+    }
+
     private Change change(int value) {
         return new Change(1, 0, 0);
     }
