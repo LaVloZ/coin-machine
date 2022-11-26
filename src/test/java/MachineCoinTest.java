7ec79ccfba8438745149d58b8c0ac838487dd720
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-26 06:10:34 +0100

    0 billet 5 for values 10, 12, 14, 16, 18 and 20

--- src/test/java/MachineCoinTest.java
+++ src/test/java/MachineCoinTest.java
@@ -41,6 +41,12 @@ public class MachineCoinTest {
             "4, 0",
             "6, 0",
             "8, 0",
+            "10, 0",
+            "12, 0",
+            "14, 0",
+            "16, 0",
+            "18, 0",
+            "20, 0",
     })
     void billet_5(int value, int expected) {
         int billet5 = change5(value);
